3cc4ab2a17587782846067fa84f12bfa4f45f6a6
LargeElmo <jimferguson@att.net>
2022-04-30 10:59:18 -0400

    Quick cofirm of revised python file
    Added variable 'apples' for future testing

--- testA.py
+++ testA.py
@@ -1,1 +1,5 @@
 print("\nLine 1 file introduced in existing folder being tracked in git.")
+
+apples = ('gala', 'winesap', 'deliscious', 'honeycrisp', 'fuji')
+
+print(f'Our available apples today are {apples}.')
